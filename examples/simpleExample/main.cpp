10ff4b660819167d3b1c4b32cdbac2a95a4207dc
JF <jf@codingfield.com>
2018-03-28 21:22:24 +0200

    Update example
    Update example with new functionality (UpDownButton, callbacks,...)

--- examples/simpleExample/main.cpp
+++ examples/simpleExample/main.cpp
@@ -1,32 +1,44 @@
 #include <M5Stack.h>
 
+#include <NTPClient.h>
 #include <Screen.h>
 #include <ButtonInfoBar.h>
 #include <StatusBar.h>
 #include <Button.h>
 #include <WidgetMosaic.h>
+#include <string>
+#include <time.h>
+#include <sys/time.h>
+#include <AppScreen.h>
+#include <UpDownButton.h>
 
 using namespace Codingfield::UI;
-Screen screen(Size(320, 240), BLACK);
+AppScreen* screen;
 StatusBar* topBar;
 ButtonInfoBar* bottomBar;
-Codingfield::UI::Button* button0; // M5STack should really use namespaces to avoid name clashes
+Codingfield::UI::Button* button0;
 Codingfield::UI::Button* button1;
-Codingfield::UI::Button* button2;
+Codingfield::UI::UpDownButton* button2;
 Codingfield::UI::Button* button3;
 Codingfield::UI::Button* button4;
 Codingfield::UI::Button* button5;
 WidgetMosaic* mosaic;
 Widget* focus;
 
+int32_t editButtonValue = 0;
+int32_t editOldButtonValue = 0;
+
 void setup() {
   M5.begin();
 
-  topBar = new StatusBar(&screen, Point(0,0), 25);
-  bottomBar = new ButtonInfoBar(&screen, Point(0, screen.GetSize().height-25), 25);
+  // Instanciate and configure all widgets
+  topBar = new StatusBar();
+  bottomBar = new ButtonInfoBar();
+  mosaic = new WidgetMosaic(3, 2);
+  screen = new AppScreen(Size(320, 240), BLACK, topBar, bottomBar, mosaic);
 
-  mosaic = new WidgetMosaic(&screen, Point(5,30), Size(320, 190));
-  focus = mosaic;
+  // Give the focus to the main screen
+  focus = screen;
 
   button0 = new Codingfield::UI::Button(mosaic);
   button0->SetBackgroundColor(BLUE);
@@ -37,10 +49,10 @@ void setup() {
   button1->SetBackgroundColor(ORANGE);
   button1->SetTextColor(BLACK);
   button1->SetText("50%");
-  button2 = new Codingfield::UI::Button(mosaic);
+  button2 = new Codingfield::UI::UpDownButton(mosaic); // Up/Down button
   button2->SetBackgroundColor(YELLOW);
   button2->SetTextColor(BLACK);
-  button2->SetText("3h15");
+  button2->SetText("0");
   button3 = new Codingfield::UI::Button(mosaic);
   button3->SetBackgroundColor(PURPLE);
   button3->SetTextColor(WHITE);
@@ -58,10 +70,62 @@ void setup() {
   topBar->SetWifiStatus(StatusBar::WifiStatuses::No_signal);
 
   bottomBar->SetButtonAText("<");
-  bottomBar->SetButtonBText("Select");
+  bottomBar->SetButtonBText("SELECT");
   bottomBar->SetButtonCText(">");
 
-  screen.Draw();
+  // Callback called by the mosaic when it changes mode (mosaic/zoom on 1 widget)
+  // We use it to update the bottom bar.
+  mosaic->SetZoomOnSelectedCallback([bottomBar](Widget* widget, bool edit) {
+    if(edit) {
+      if(widget->IsEditable()){
+        bottomBar->SetButtonAText("-");
+        bottomBar->SetButtonBText("APPLY");
+        bottomBar->SetButtonCText("+");
+      } else {
+        bottomBar->SetButtonAText("");
+        bottomBar->SetButtonBText("BACK");
+        bottomBar->SetButtonCText("");
+      }
+    } else {
+      bottomBar->SetButtonAText("<");
+      bottomBar->SetButtonBText("SELECT");
+      bottomBar->SetButtonCText(">");
+    }
+  });
+
+  // Configure callback to be called when the user wants to increment the value
+  // of button2
+  button2->SetUpCallback([&editButtonValue](UpDownButton* w) {
+    editButtonValue++;
+    w->SetText(String(editButtonValue).c_str());
+    return true;
+  });
+
+  // Configure callback to be called when the user wants to decrement the value
+  // of button2
+  button2->SetDownCallback([&editButtonValue](UpDownButton* w) {
+    editButtonValue--;
+    w->SetText(String(editButtonValue).c_str());
+    return true;
+  });
+
+  // Configure callback to be called when the user wants to apply the value
+  // of button2
+  button2->SetApplyCallback([&editButtonValue, &editOldButtonValue](UpDownButton* w) {
+    editOldButtonValue = editButtonValue;
+    return false;
+  });
+
+  // Configure callback to be called when the user wants to cancel modification
+  // of the value of button2
+  button2->SetCancelCallback([&editButtonValue](UpDownButton* w) {
+    editButtonValue = editOldButtonValue;
+    w->SetText(String(editButtonValue).c_str());
+    return true;
+  });
+
+  // Draw the screen and all its children
+  screen->Draw();
 }
 
 uint32_t loopCount = 0;
@@ -69,16 +133,16 @@ uint32_t temperature = 10;
 uint32_t percent = 0;
 std::string state = "BUSY";
 int32_t wifiStatusIndex;
-int rssi = -100;
-int seconds = 0;
 std::vector<StatusBar::WifiStatuses> wifiStatus {StatusBar::WifiStatuses::No_signal,
                                                  StatusBar::WifiStatuses::Weak,
                                                  StatusBar::WifiStatuses::Medium,
                                                  StatusBar::WifiStatuses::Full};
 int32_t uptimeHours=0;
+bool longPush = false;
 void loop() {
   M5.update();
 
+  // Update values displayd on the screen (status bar, buttons,...)
   if((loopCount % 50) == 0) {
     if(temperature < 100)
       temperature++;
@@ -99,43 +163,53 @@ void loop() {
     topBar->SetUptime(uptimeHours);
 
     char strftime_buf[64];
-    if(seconds < 60)
-      seconds++;
-    else
-      seconds = 0;
-    snprintf(strftime_buf, 64, "%02d:%02d:%02d", 1, 2, seconds);
+    snprintf(strftime_buf, 64, "%02d:%02d:%02d", 12, 14, 59);
     topBar->SetDateTime(strftime_buf);
-
-    if(rssi < 0)
-      rssi++;
-    else
-      rssi = -100;
-    if(rssi >= -55)
-      topBar->SetWifiStatus(StatusBar::WifiStatuses::Full);
-    else if(rssi >= -75)
-      topBar->SetWifiStatus(StatusBar::WifiStatuses::Medium);
-    else if(rssi >= -85)
-      topBar->SetWifiStatus(StatusBar::WifiStatuses::Weak);
-    else
-      topBar->SetWifiStatus(StatusBar::WifiStatuses::No_signal);
   }
 
   if((loopCount % 100) == 0) {
     if(state == "BUSY") state = "IDLE";
     else state = "BUSY";
     button5->SetText(state);
+
+    auto rssi =WiFi.RSSI();
+    if(rssi >= -55) {
+      topBar->SetWifiStatus(StatusBar::WifiStatuses::Full);
+    } else if(rssi >= -75) {
+      topBar->SetWifiStatus(StatusBar::WifiStatuses::Medium);
+    } else if(rssi >= -85) {
+      topBar->SetWifiStatus(StatusBar::WifiStatuses::Weak);
+    } else {
+      topBar->SetWifiStatus(StatusBar::WifiStatuses::No_signal);
+    }
   }
 
-  if(M5.BtnA.wasPressed())
+  // Notify the widgets that physical buttons are pressed
+  if(M5.BtnA.wasPressed()) {
     focus->OnButtonAPressed();
+  }
 
-  if(M5.BtnB.wasPressed())
-    focus->OnButtonBPressed();
+  if(M5.BtnB.pressedFor(1000)) {
+    if(!longPush) {
+      focus->OnButtonBLongPush();
+      longPush = true;
+    }
+  }
+  else if(M5.BtnB.wasReleased()) {
+    if(!longPush) {
+      focus->OnButtonBPressed();
+    }
+    else {
+      longPush = false;
+    }
+  }
 
-  if(M5.BtnC.wasPressed())
+  if(M5.BtnC.wasPressed()) {
     focus->OnButtonCPressed();
+  }
 
-  screen.Draw();
+  // Redraw the screen
+  screen->Draw();
 
   loopCount++;
   delay(10);
